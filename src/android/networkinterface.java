245655be6b8bfda2decab558ae9083df084f943e
Guy Rombaut <guy.rombaut@avg.com>
2016-04-07 13:34:50 +0200

    android wifi extended info

--- src/android/networkinterface.java
+++ src/android/networkinterface.java
@@ -6,12 +6,16 @@ import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.net.DhcpInfo;
+import android.provider.Settings;
 
 public class networkinterface extends CordovaPlugin {
 	public static final String GET_IP_ADDRESS="getIPAddress";
+	public static final String GET_NETWORK_INFO="getNetworkInfo";
 
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -26,14 +30,21 @@ public class networkinterface extends CordovaPlugin {
 				callbackContext.success(ip);
 				return true;
 			}
+			else if(GET_NETWORK_INFO.equals(action)) {
+				JSONObject networkInfo = getNetworkInfo();
+				callbackContext.success(networkInfo);
+				return true;
+			}
+			
 			callbackContext.error("Error no such method '" + action + "'");
 			return false;
+			
 		} catch(Exception e) {
-			callbackContext.error("Error while retrieving the IP address. " + e.getMessage());
+			callbackContext.error("Error while retrieving network information. " + e.getMessage());
 			return false;
 		}
 	}
-
+	
 	private String getIPAddress() {
 		WifiManager wifiManager = (WifiManager) cordova.getActivity().getSystemService(Context.WIFI_SERVICE);
 		WifiInfo wifiInfo = wifiManager.getConnectionInfo();
@@ -48,5 +59,43 @@ public class networkinterface extends CordovaPlugin {
 		);
 
 		return ipString;
+	}
+	
+	private String intToIp(int intip) {
+		String ipString = String.format(
+			"%d.%d.%d.%d",
+			(intip & 0xff),
+			(intip >> 8 & 0xff),
+			(intip >> 16 & 0xff),
+			(intip >> 24 & 0xff)
+		);
+				
+		return ipString;
+	}
+	
+	private JSONObject getNetworkInfo() {
+		WifiManager wifiManager = (WifiManager) cordova.getActivity().getSystemService(Context.WIFI_SERVICE);
+		WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+		DhcpInfo dhcpInfo = wifiManager.getDhcpInfo();
+		
+		JSONObject dhcpJson = new JSONObject();
+		
+		try { 
+            dhcpJson.put("staticIp", intToIp(dhcpInfo.ipAddress));
+            dhcpJson.put("wifiAddress", intToIp(wifiInfo.getIpAddress()));
+            dhcpJson.put("netmask", intToIp(dhcpInfo.netmask));
+            dhcpJson.put("gateway", intToIp(dhcpInfo.gateway));
+            dhcpJson.put("dns1", intToIp(dhcpInfo.dns1));
+            dhcpJson.put("dns2", intToIp(dhcpInfo.dns2));
+            dhcpJson.put("serverAddress", intToIp(dhcpInfo.serverAddress));
+            dhcpJson.put("mac", String.valueOf(wifiInfo.getMacAddress()));
+            dhcpJson.put("ssid", wifiInfo.getSSID());
+            int useStatic = Settings.System.getInt(cordova.getActivity().getContentResolver(), Settings.System.WIFI_USE_STATIC_IP);
+            dhcpJson.put("use_static_ip", useStatic == 1 ? "Static" : "Auto");
+		} catch (JSONException ex) {
+            Log.e("getDHCPInfo Error", "JSON Error:" + ex.getMessage());
+        } finally {
+            return dhcpJson;
+        }
 	}
 }
