4dec59f4a50b88dcfd43288743dca44caef93115
Guy Rombaut <guy.rombaut@avg.com>
2016-04-08 10:49:19 +0200

    remove unused plugin

--- src/android/networkinterface.java
+++ src/android/networkinterface.java
@@ -4,7 +4,6 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.api.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
